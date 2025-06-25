d9876baf00ff8fa7903dab4430608ca088a47052
nwukodavid <nwukoakachukwu@gmail.com>
2025-06-25 23:31:51 +0100

    Add missing auth route

---
+++ routes/auth.js
@@ -0,0 +1,11 @@
+const express = require('express');
+const router = express.Router();
+const { registerUser, loginUser, forgotPassword, resetPassword } = require('../controllers/userController');
+
+// Routes
+router.post('/register', registerUser);
+router.post('/login', loginUser);
+router.post('/forgot-password', forgotPassword);
+router.post('/reset-password', resetPassword);
+
+module.exports = router;
